238bd410aabe5e901573af8823ae20de998f446b
keis8221 <kei_s8221@icloud.com>
2024-04-11 23:57:00 +0900

    remove unnecessary code

--- pkg/adaptor/http/health_check.go
+++ pkg/adaptor/http/health_check.go
@@ -11,13 +11,6 @@ type healthCheckResponse struct {
 	Message string `json:"message"`
 }
 
-// for health check
-//
-//	{
-//	    "message": "Hello, C Team. you've requested: /health_check"
-//	}
-//
-// will return
 func healthCheck(c echo.Context) error {
 	message := fmt.Sprintf("Hello! you've requested: %s", c.Path())
 	return c.JSON(
